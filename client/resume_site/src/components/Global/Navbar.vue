877683d2a8125866ed7a8b5c6f2a49ec03295d09
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-28 02:02:53 -0600

    added navbar to global components

---
+++ client/resume_site/src/components/Global/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="navbar">
+        <nav>
+            <ul class="nav-list">
+                <li class="nav-item">Home</li>
+                <li class="nav-item">Resume</li>
+                <li class="nav-item">About</li>
+                <li class="nav-item">Projects & Samples</li>
+                <li class="nav-item">Services</li>
+                <li class="nav-item">Contact</li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+<style scoped>
+    .navbar {
+        width: 100%;
+    }
+
+    .nav-list {
+        display: flex;
+        list-style-type: none;
+        justify-content: flex-end;
+        padding-right: 30px;
+        font-size: 20px;
+    }
+
+    .nav-item {
+        color: #7aa3c1;
+        margin: 0px 10px;
+    }
+</style>
