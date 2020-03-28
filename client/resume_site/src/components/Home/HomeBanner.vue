2e5f1aff61412ee53ae7993e86bb1a3c18859833
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-28 09:10:41 -0600

    added tagline

--- client/resume_site/src/components/Home/HomeBanner.vue
+++ client/resume_site/src/components/Home/HomeBanner.vue
@@ -2,10 +2,10 @@
     <div class="banner">
         <Navbar />
         <h1 class="name">Brandon Ellis</h1>
-        <h3>Web Developer | Digital Analyst</h3>
+        <h3>const { frontend, backend, fullStack } = require('web_developer')</h3>
         <div>
-            <button>Resume</button>
-            <button>Inquire</button>
+            <button class="resumebtn">Resume</button>
+            <button class="inquirebtn">Inquire</button>
         </div>
     </div>
 </template>
@@ -23,7 +23,9 @@ export default {
 
 <style scoped>
     .banner {
-        background-image: url("https://res.cloudinary.com/btdigital/image/upload/v1574379949/Malaysia-towers-abstract-color-2_lxpqeb.png");
+        background-image: 
+            linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(0,0,0, 0.73)),
+            url("https://res.cloudinary.com/btdigital/image/upload/v1574379949/Malaysia-towers-abstract-color-2_lxpqeb.png");
         background-size: 100vw;
         background-repeat: no-repeat;
         width: 100%;
@@ -32,10 +34,20 @@ export default {
     }
 
     button {
-        padding: 10px;
+        padding: 20px;
         font-size: 16px;
         border-radius: 50px 50px;
         margin: 0px 5px;
+    }
+
+    .resumebtn {
+        background-color: white;
+        color: #7aa3c1;
+    }
+
+    .inquirebtn {
+        background-color: rgba(29,29,29, 0.6);
+        color: white;
     }
 
     .name {
