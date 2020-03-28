ae1eb604a0216a8222699541dfef6946b3897e00
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-27 22:04:37 -0600

    removed generic content, added a todos component

--- resume_site/src/App.vue
+++ resume_site/src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo 2",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo 3",
+          completed: false
+        },
+      ]
+    }
   }
 }
 </script>
@@ -23,6 +42,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
