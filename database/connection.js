905b9a97503df3988926e3d4351dcae27a619c3d
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-28 20:36:24 -0600

    exported connection

--- database/connection.js
+++ database/connection.js
@@ -1,2 +1,14 @@
 const mysql = require('mysql');
 const config = require('./config.js');
+
+const connection = mysql.createConnection(config);
+
+module.exports.connection = connection;
+
+module.exports.connection.connect(err => {
+    if (err) {
+        console.log('Error connecting to database', err.stack);
+    } else {
+        console.log('Database connected successfully');
+    }
+});
