2745e39a73268d594eb8ca07418a16f2b1a5d723
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-30 14:43:00 -0600

    added all calls

---
+++ client/resume_site/src/api/webApps.js
@@ -0,0 +1,84 @@
+const webAppsUrl = 'http://localhost:5000/api/web-applications';
+
+class WebAppsApi {
+    static getWebApps() {
+        return fetch(webAppsUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                return err;
+            });
+    }
+
+    static postWebApps(application) {
+        return fetch(`${webAppsUrl}/new`, {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(application)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+
+    static putWebApps(id, columnName, value) {
+
+        let payload = {
+            id: id,
+            columnName: columnName,
+            value: value
+        };
+
+        return fetch(`${webAppsUrl}/${id}`, {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+
+    static deleteWebApps(id) {
+        return fetch(`${webAppsUrl}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+}
+
+export default WebAppsApi;
