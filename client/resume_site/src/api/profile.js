209a62068163f09bbbcdbd0844d552792234889c
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-30 14:34:50 -0600

    separated api functionality by each type of data

---
+++ client/resume_site/src/api/profile.js
@@ -0,0 +1,86 @@
+
+const profileUrl = 'http://localhost:5000/api/profiles';
+
+class ProfileApi {
+    static getProfile(id) {
+        return fetch(`${profileUrl}/${id}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+
+    static postProfile(profile) {
+        return fetch(`${profileUrl}/new`, {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(profile)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+
+    static putProfile(id, columnName, value) {
+
+        let payload = {
+            id: id,
+            columnName: columnName,
+            value: value
+        };
+
+        return fetch(`${profileUrl}/${id}`, {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+
+    static deleteProfile(id) {
+        return fetch(`${profileUrl}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+}
+
+export default ProfileApi;
---
+++ client/resume_site/src/api/testimonials.js
@@ -0,0 +1,84 @@
+const testimonialUrl = 'http://localhost:5000/api/testimonials';
+
+class TestimonialsApi {
+    static getTestimonials() {
+        return fetch(testimonialUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                return err;
+            });
+    }
+
+    static postTestimonial(profile) {
+        return fetch(`${testimonialUrl}/new`, {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(profile)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+
+    static putTestimonial(id, columnName, value) {
+
+        let payload = {
+            id: id,
+            columnName: columnName,
+            value: value
+        };
+
+        return fetch(`${testimonialUrl}/${id}`, {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+
+    static deleteTestimonial(id) {
+        return fetch(`${testimonialUrl}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.log(err);
+                return err;
+            });
+    }
+}
+
+export default TestimonialsApi;
