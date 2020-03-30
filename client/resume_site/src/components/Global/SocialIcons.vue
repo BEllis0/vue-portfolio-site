2e7565f0cc6d2e09329f86311f02cd5a437e685b
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-29 22:32:58 -0600

    added social icons comp design

---
+++ client/resume_site/src/components/Global/SocialIcons.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="social-icons">
+        <a href="https://www.linkedin.com/in/brandon-t-ellis">
+            <img class="linkedin icon" src="https://image.flaticon.com/icons/svg/145/145807.svg" />
+        </a>
+        <a href="https://www.youtube.com/channel/UCvxSwErbNJ_-ZvK1MqkBUug">
+            <img class="youtube icon" src="https://image.flaticon.com/icons/svg/187/187209.svg" />
+        </a>
+        <a href="https://twitter.com/TravelAdvocate0">
+            <img class="twitter icon" src="https://image.flaticon.com/icons/svg/145/145812.svg" />
+        </a>
+        <a href="https://www.facebook.com/brandon.ellis.505">
+            <img class="facebook icon" src="https://image.flaticon.com/icons/svg/145/145802.svg" />
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SocialIcons"
+}
+</script>
+
+<style scoped>
+    .icon {
+        margin: 0px 2px;
+    }
+
+    .facebook, .linkedin, .twitter, .youtube {
+        width: 25px;
+        height: 25px;
+    }
+</style>
