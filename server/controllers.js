b8a26cf40105207a871cbf98278b13c5fbf0b059
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-30 14:36:04 -0600

    added controllers for each route

--- server/controllers.js
+++ server/controllers.js
@@ -1,10 +1,60 @@
 // const models = require('./models');
 
 module.exports = {
-    getWebApps: (req, res) => {
-        res.send('get web apps');
+    get: {
+        getProfile: (req, res) => {
+            res.json('profile fetched');
+        },
+        getWebApps: (req, res) => {
+            res.json('web apps fetched');
+        },
+        getWebDesigns: (req, res) => {
+            res.json('web designs fetched');
+        },
+        getTestimonials: (req, res) => {
+            res.json('testimonials fetched');
+        }
     },
-    getWebDesigns: (req, res) => {
-        res.send('get web designs');
+    post: {
+        postProfile: (req, res) => {
+            res.json('profile added');
+        },
+        postWebApps: (req, res) => {
+            res.json('web app added');
+        },
+        postWebDesigns: (req, res) => {
+            res.json('web design added');
+        },
+        postTestimonials: (req, res) => {
+            res.json('testimonial added');
+        }
+    },
+    put: {
+        putProfile: (req, res) => {
+            res.json('profile changed');
+        },
+        putWebApps: (req, res) => {
+            res.json('web apps changed');
+        },
+        putWebDesigns: (req, res) => {
+            res.json('web designs changed');
+        },
+        putTestimonials: (req, res) => {
+            res.json('testimonials changed');
+        }
+    },
+    delete: {
+        deleteProfile: (req, res) => {
+            res.json('profile deleted');
+        },
+        deleteWebApps: (req, res) => {
+            res.json('web apps deleted');
+        },
+        deleteWebDesigns: (req, res) => {
+            res.json('web designs deleted');
+        },
+        deleteTestimonials: (req, res) => {
+            res.json('testimonials deleted');
+        }
     }
 };
