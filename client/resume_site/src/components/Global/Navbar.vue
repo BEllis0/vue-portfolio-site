bbffe1813df1ced2266b6dfc611dc563b92a8d21
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-29 22:31:05 -0600

    mobile layout and hover

--- client/resume_site/src/components/Global/Navbar.vue
+++ client/resume_site/src/components/Global/Navbar.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="navbar">
         <nav>
-            <ul class="nav-list">
+            <ul class="nav-list desktop">
                 <li class="nav-item">Home</li>
                 <li class="nav-item">Resume</li>
                 <li class="nav-item">About</li>
                 <li class="nav-item">Projects & Samples</li>
                 <li class="nav-item">Services</li>
                 <li class="nav-item">Contact</li>
+            </ul>
+            <ul class="nav-list mobile-tablet">
+                <div>
+                <li class="nav-item">Home</li>
+                <li class="nav-item">Resume</li>
+                <li class="nav-item">About</li>
+                </div>
+                <div>
+                <li class="nav-item">Projects & Samples</li>
+                <li class="nav-item">Services</li>
+                <li class="nav-item">Contact</li>
+                </div>
             </ul>
         </nav>
     </div>
@@ -22,6 +34,7 @@ export default {
 <style scoped>
     .navbar {
         width: 100%;
+        padding: 20px 0px;
     }
 
     .nav-list {
@@ -34,8 +47,44 @@ export default {
         padding: 5px;
     }
 
+    .mobile-tablet {
+        visibility: hidden;
+        display: none;
+    }
+
+    .desktop {
+        visibility: initial;
+    }
+
+    .nav-item:hover {
+        border-bottom: 1px solid #7aa3c1;
+        font-weight: 650;
+    }
+
     .nav-item {
         color: #7aa3c1;
         margin: 0px 10px;
+    }
+
+    @media screen and (max-width: 850px) {
+        .navbar {
+            padding: 10px 0px;
+        }
+
+        .nav-list {
+            justify-content: center;
+            font-size: 14px;
+            padding-right: 0px; 
+        }
+
+        .mobile-tablet {
+            visibility: initial;
+            display: flex;
+        }
+
+        .desktop {
+            visibility: hidden;
+            display: none;
+        }
     }
 </style>
