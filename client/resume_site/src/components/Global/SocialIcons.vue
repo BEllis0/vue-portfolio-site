c2aa0d473377c3bc940aa917136ac7d7439d794e
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-30 14:36:41 -0600

    added _blank for each button

--- client/resume_site/src/components/Global/SocialIcons.vue
+++ client/resume_site/src/components/Global/SocialIcons.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="social-icons">
-        <a href="https://www.linkedin.com/in/brandon-t-ellis">
+        <a href="https://www.linkedin.com/in/brandon-t-ellis" target="_blank">
             <img class="linkedin icon" src="https://image.flaticon.com/icons/svg/145/145807.svg" />
         </a>
-        <a href="https://www.youtube.com/channel/UCvxSwErbNJ_-ZvK1MqkBUug">
+        <a href="https://www.youtube.com/channel/UCvxSwErbNJ_-ZvK1MqkBUug" target="_blank">
             <img class="youtube icon" src="https://image.flaticon.com/icons/svg/187/187209.svg" />
         </a>
-        <a href="https://twitter.com/TravelAdvocate0">
+        <a href="https://twitter.com/TravelAdvocate0" target="_blank">
             <img class="twitter icon" src="https://image.flaticon.com/icons/svg/145/145812.svg" />
         </a>
-        <a href="https://www.facebook.com/brandon.ellis.505">
+        <a href="https://www.facebook.com/brandon.ellis.505" target="_blank">
             <img class="facebook icon" src="https://image.flaticon.com/icons/svg/145/145802.svg" />
         </a>
     </div>
