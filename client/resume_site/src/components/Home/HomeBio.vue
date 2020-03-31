d8178243fbf4c0bcc6ca0b02a2f1748e4132f52c
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-31 15:34:17 -0600

    finished bio section, added mobile design

--- client/resume_site/src/components/Home/HomeBio.vue
+++ client/resume_site/src/components/Home/HomeBio.vue
@@ -3,19 +3,22 @@
         <div class="main-page-container">
             <Navbar />
             <div class="flex">
-                <section>
+                <div class="bio-left">
+                    <div class="profile-left">
                     <img class="headshot-img" alt="Headshot of Brandon Ellis" src="http://www.brandontellis.com/wp-content/uploads/2019/11/26017635.jpeg" />
                     <SocialIcons />
-                </section>
-                <section>
-                    <h2>About Me</h2>
+                    <SocialRepos />
+                    </div>
+                </div>
+                <div class="bio-right">
+                    <h2 class="light-blue-text">About Me</h2>
                     <h3>Web Developer | Digital Analyst</h3>
                     <p class="grey-text">I am a full stack web developer with a background at the crossroads of web development and digital marketing. After receiving formal training at Hack Reactor (at Galvenize), I am eager to make a full transition into the world of software development. Though I am a recent graduate of Hack Reactor, the introduction to ‘coder’s high’ began a few years ago after discovering development and building my own consulting business, BT Digital, where I’ve focused on building websites and online presence for small nonprofits and businesses. I am passionate about technology and helping under-resourced groups build their capacity for growth.</p>
                     <br />
                     <p class="grey-text">Outside of web development and digital marketing, I’ve had the pleasure of providing logistical and on-site support for – and presented independent research at – international academic conferences in Malaysia, Hungary, and multiple locations in China.</p>
                     <br />
                     <p class="grey-text">Though my experience is currently within the nonprofit sector, I am actively seeking creative opportunities in organizations/companies that offer the chance to grow and make an impact. </p>
-                </section>
+                </div>
             </div>
         </div>
     </div>
@@ -24,11 +27,13 @@
 <script>
 import Navbar from '../Global/Navbar.vue';
 import SocialIcons from '../Global/SocialIcons.vue';
+import SocialRepos from '../Global/SocialRepos.vue';
 
 export default {
     name: "HomeBio",
     components: {
         SocialIcons,
+        SocialRepos,
         Navbar
     }
 }
@@ -44,9 +49,24 @@ export default {
     }
 
     .headshot-img {
-        width: 120px;
-        height: 120px;
+        width: 130px;
+        height: 130px;
         border-radius: 50% 50%;
+    }
+
+    .bio-left {
+        width: 30%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .profile-left {
+        padding-top: 30px;
+    }
+
+    .bio-right {
+        width: 70%;
     }
 
 </style>
