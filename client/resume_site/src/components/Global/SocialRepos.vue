805d33493eb0462396b550185db69c9f4df48817
Brandon Ellis <brandonellis.email@gmail.com>
2020-03-31 15:32:37 -0600

    added social repos comp design

---
+++ client/resume_site/src/components/Global/SocialRepos.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="block">
+        <a href="https://codepen.io/BrandonEllis" target="_blank">
+            <img class="codepen icon" src="http://www.brandontellis.com/wp-content/uploads/2019/11/codepen-wordmark-display-inside-white@10x.png" />
+        </a>
+        <a href="https://github.com/BEllis0" target="_blank">
+            <img class="github icon" src="http://www.brandontellis.com/wp-content/uploads/2019/11/GitHub_Logo_White.png" />
+        </a>
+        <a href="https://public.tableau.com/profile/brandon3955#!/" target="_blank">
+            <img class="tableau icon" src="http://www.brandontellis.com/wp-content/uploads/2019/11/logo.png" />
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SocialRepos"    
+}
+</script>
+
+<style scoped>
+    .icon {
+        display: block;
+        max-width: 100px;
+        margin: 5px;
+    }
+
+</style>
