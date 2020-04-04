dfc29b8afd03947fd0ec211cc8c3d08f2856eb31
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-04 17:53:48 -0600

    changed to pool connection

--- database/connection.js
+++ database/connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql');
 const config = require('./config.js');
 
-const connection = mysql.createConnection(config);
+const connection = mysql.createPool(config);
 
 module.exports.connection = connection;
 
-module.exports.connection.connect(err => {
-    if (err) {
-        console.log('Error connecting to database', err.stack);
-    } else {
-        console.log('Database connected successfully');
-    }
-});
+// module.exports.connection.connect(err => {
+//     if (err) {
+//         console.log('Error connecting to database', err.stack);
+//     } else {
+//         console.log('Database connected successfully');
+//     }
+// });
