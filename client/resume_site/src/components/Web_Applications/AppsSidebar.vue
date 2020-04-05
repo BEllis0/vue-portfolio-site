a7ae12c1c34e00646f2af45d39ca8da36c7b473e
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-05 16:38:36 -0600

    sidebar comp, updates to sizing and positioning

---
+++ client/resume_site/src/components/Web_Applications/AppsSidebar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="sidebar">
+        <h3 class="sidebar-title">Applications</h3>
+        <div class="sidebar-application-items">
+            <h4 v-for="app in applications" :key="app.id">
+                {{ app.name }}
+            </h4>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppsSidebar",
+    data: () => ({
+        applications: [
+            { id: 1, name: "Watchlists.io | Stock App", url_link: 'http://watchlists.io/', anchor_link: '#watchlists' }
+        ]
+    })
+}
+</script>
+
+<style scoped>
+    .sidebar {
+        padding: 10px 0px;
+        padding-left: 20px;
+    }
+
+    .sidebar-title {
+        border-bottom: 1px solid var(--light-blue);
+    }
+</style>
