b1998fe8d8119b91973138337349cdd67bee3c27
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-05 16:39:04 -0600

    passing props to item

---
+++ client/resume_site/src/components/Web_Applications/AppsItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{ app.name }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppsItem",
+    props: ["app"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/resume_site/src/components/Web_Applications/AppsList.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <div v-for="app in appItems" :key="app.id">
+        <AppsItem v-bind:app="app" />
+    </div>
+</div>
+</template>
+
+<script>
+import AppsItem from './AppsItem.vue';
+
+export default {
+    name: "AppsList",
+    components: {
+        AppsItem
+    },
+    data: () => ({
+        appItems: [
+            { id: 1, name: "Watchlists.io | Stock App", url_link: 'http://watchlists.io/', images: []}
+        ]
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
