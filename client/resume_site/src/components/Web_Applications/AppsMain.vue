52a89f9c4048485665ae4db9f9a00293aa4efc1d
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-05 16:39:28 -0600

    main section comp, updates to sizing and positioning

---
+++ client/resume_site/src/components/Web_Applications/AppsMain.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="main">
+        <AppsList />
+        <div class="test"></div>
+        <div class="test"></div>
+        <div class="test"></div>
+        <div class="test"></div>
+    </div>
+</template>
+
+<script>
+import AppsList from './AppsList.vue';
+
+export default {
+    name: "AppsMain",
+    components: {
+        AppsList
+    }
+}
+</script>
+
+<style scoped>
+    .test {
+        width: 50%;
+        height: 600px;
+    }
+</style>
