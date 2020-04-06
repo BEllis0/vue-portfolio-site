8e387dbf3da7418ffd472e707909ea74d98461a5
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-06 00:26:09 -0600

    added format for app item, passing in props

--- client/resume_site/src/components/Web_Applications/AppsItem.vue
+++ client/resume_site/src/components/Web_Applications/AppsItem.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
         <h1>{{ app.name }}</h1>
+        <h4>Details</h4>
+        <p>{{ app.details }}</p>
+        <h4>Dependencies</h4>
+        <p>{{ app.dependencies }}</p>
+        <div class="image-section">
+            <div v-for="image in app.images" :key="image.link">
+                <img class="app-image" :src="image.link" />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,5 +21,14 @@ export default {
 </script>
 
 <style scoped>
+    .image-section {
+        display: flex;
+        flex-wrap: wrap;
+    }
 
+    .app-image {
+        width: 250px;
+        height: auto;
+        margin: 0px 5px;
+    }
 </style>
