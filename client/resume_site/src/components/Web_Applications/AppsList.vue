d8881c9e4375f6fd66a0ad1ba0d404e42e5fc820
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-06 00:26:51 -0600

    passing data in to item

--- client/resume_site/src/components/Web_Applications/AppsList.vue
+++ client/resume_site/src/components/Web_Applications/AppsList.vue
@@ -16,7 +16,40 @@ export default {
     },
     data: () => ({
         appItems: [
-            { id: 1, name: "Watchlists.io | Stock App", url_link: 'http://watchlists.io/', images: []}
+            { 
+                id: 1,
+                name: "Watchlists.io | Stock App",
+                url_link: 'http://watchlists.io/',
+                details: `Built using the MERN stack (Mongoose, Express, React & Node). 
+                            Uses Alpha Vantage API for real time and historical stock data and Google News API for stock news. 
+                            Designed with Material UI. 
+                            Includes password hashing with bcryptjs. 
+                            Uses Google Recaptcha to secure forms. 
+                            Throttling and debouncing with lodash.`,
+                dependencies: `Backend: bcrypt, cors, dotenv, express, mongoose, newsapi, nodemon, pusher, validator. 
+                            Frontend: axios, @material-ui/core, @material-ui/icons, @material-ui/lab, react, react-dom, react-router, react-router-dom, chart.js, react-chartjs-2, lodash, http-proxy-middleware, react-recaptcha-google`,
+                images: [
+                    {link: 'https://res.cloudinary.com/btdigital/image/upload/v1585775160/watchlist-scaled.jpg'},
+                    {link: 'https://res.cloudinary.com/btdigital/image/upload/v1585775161/watchlist-search-scaled.jpg'},
+                    {link: 'https://res.cloudinary.com/btdigital/image/upload/v1585775162/watchlist-signup.jpg'}
+                ]
+            },
+            { 
+                id: 2,
+                name: "Amazon Prime Video Clone",
+                url_link: 'https://amazon-review-service.herokuapp.com/',
+                details: `Built using the MERN stack (Mongoose, Express, React & Node). 
+                            Uses Alpha Vantage API for real time and historical stock data and Google News API for stock news. 
+                            Designed with Material UI. 
+                            Includes password hashing with bcryptjs. 
+                            Uses Google Recaptcha to secure forms. 
+                            Throttling and debouncing with lodash.`,
+                dependencies: `Backend: bcrypt, cors, dotenv, express, mongoose, newsapi, nodemon, pusher, validator. 
+                            Frontend: axios, @material-ui/core, @material-ui/icons, @material-ui/lab, react, react-dom, react-router, react-router-dom, chart.js, react-chartjs-2, lodash, http-proxy-middleware, react-recaptcha-google`,
+                images: [
+                    'https://res.cloudinary.com/btdigital/image/upload/v1585775164/amazon-scaled.jpg'
+                ]
+            }
         ]
     })
 }
