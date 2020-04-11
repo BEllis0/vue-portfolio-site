62d1e5b2ddf02795289c880d86f39a469c2b1396
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-11 16:43:25 -0600

    added link to data visuals page

--- client/resume_site/src/components/Global/Navbar.vue
+++ client/resume_site/src/components/Global/Navbar.vue
@@ -70,7 +70,7 @@ export default {
         items: [
             { title: 'Web Design', link: '/web-designs' },
             { title: 'Web Applications', link: '/web-apps' },
-            { title: 'Data Visualization', link: '/web-apps' },
+            { title: 'Data Visualization', link: '/data-visualization' },
         ]
     }),
 }
