79ffa4734ce3f0be6b319a9c6216bbf0f6637dda
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-14 11:42:32 -0600

    test coverage

--- client/resume_site/tests/unit/homebanner.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import HomeBanner from '../../src/components/Home/HomeBanner.vue';
-
-describe('Home Banner', () => {
-  it('renders properly', () => {
-    const Banner = shallowMount(HomeBanner);
-    expect(Banner.exists()).toBeTruthy();
-  });
-});
