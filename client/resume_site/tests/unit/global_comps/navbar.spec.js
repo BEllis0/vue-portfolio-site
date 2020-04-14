7a802d6613614f2fff79c50869ac79e24bd45a8f
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-14 14:18:35 -0600

    updated test for navbar

--- client/resume_site/tests/unit/global_comps/navbar.spec.js
+++ client/resume_site/tests/unit/global_comps/navbar.spec.js
@@ -3,34 +3,30 @@ import Navbar from '../../../src/components/Global/Navbar.vue';
 
 describe('Navbar', () => {
 
-    it('renders properly', () => {
-        const nav = shallowMount(Navbar, {
+    const nav = shallowMount(Navbar, {
         propsData: {
             page: 'Home',
-            over: false
+            over: false,
+            updateContact: function() {
+                this.over = true
+            }
         },
         stubs: ['router-link', 'v-menu', 'v-list', 'v-list-item', 'v-list-item-title']
-        });
+    });
 
+    it('renders properly', () => {
         expect(nav.exists()).toBeTruthy();
     });
 
+
   it('contact button triggers method', () => {
-        const nav = shallowMount(Navbar, {
-            propsData: {
-                page: 'Home',
-                over: false
-            },
-            stubs: ['router-link', 'v-menu', 'v-list', 'v-list-item', 'v-list-item-title']
-        });
+        //before click
+        expect(nav.props().over).toBeFalsy();
 
-        const handleUpdate = function() {
-            this.over = true
-        };
-
-        nav.setMethods({ updateContact: handleUpdate });
-
+        //target button
         let contactBtn = nav.find('.contact-link');
-        console.log(contactBtn)
+        // contactBtn.trigger('click');
+
+        // expect(nav.props().over).toBeTruthy();
   });
 });
