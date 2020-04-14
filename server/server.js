f5fe7d43f25c58d97f63bee46e89bf7a44a7233a
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-13 20:39:53 -0600

    ran build script and added static files to server

--- server/server.js
+++ server/server.js
@@ -2,22 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 
 //routes import
 const routes = require('./routes');
 
 const app = express();
 
+//serve static files
+app.use(express.static(path.join(__dirname, '../client/resume_site/dist')));
+
 // ==== middleware
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-
-// === proxy
-// app.use('/api/profiles/*', createProxyMiddleware({
-//     target: 'http://localhost:5000',
-//     changeOrigin: true,
-// }));
 
 //routes
 app.use('/api', routes);
