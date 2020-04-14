35120c547f358d6e98af78d287f1cffff58b7395
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-14 14:34:36 -0600

    test updates

--- client/resume_site/tests/unit/web_designs/designsitem.spec.js
+++ client/resume_site/tests/unit/web_designs/designsitem.spec.js
@@ -1,8 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
 import DesignsItem from '../../../src/components/Web_Designs/DesignsItem.vue';
 
+Vue.use(lazy);
+
 const designsItem = shallowMount(DesignsItem, {
-    stubs: ['router-link'],
+    stubs: ['router-link', 'v-lazy'],
     propsData: {
         design: {
           id: 1,
