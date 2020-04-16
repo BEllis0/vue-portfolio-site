a3182419da26732f1bc5e9cf8d7840f7a84ba218
Brandon Ellis <brandonellis.email@gmail.com>
2020-04-16 12:20:42 -0600

    added alt text to images

--- client/resume_site/src/components/Home/HomeClients.vue
+++ client/resume_site/src/components/Home/HomeClients.vue
@@ -3,10 +3,10 @@
         <div class="main-page-container">
             <h2 class="light-blue-text section-title">CLIENTS</h2>
             <div class="flex">
-                <img class="clientImg" src="https://res.cloudinary.com/btdigital/image/upload/v1574379946/1-3_gn3p4q.png" />
-                <img class="clientImg" src="https://res.cloudinary.com/btdigital/image/upload/v1574379945/2-3_eq4tsg.png" />
-                <img class="clientImg" src="https://res.cloudinary.com/btdigital/image/upload/v1574379944/4-4_lmhap5.png" />
-                <img class="clientImg" src="https://res.cloudinary.com/btdigital/image/upload/v1574379945/3-3_a0eceo.png" />
+                <img class="clientImg" alt="Logos for Center for Women's Health Research and The Denver Foundation" src="https://res.cloudinary.com/btdigital/image/upload/v1574379946/1-3_gn3p4q.png" />
+                <img class="clientImg" alt="Logos for Women With A Cause Foundation and National Retail Concept Partners" src="https://res.cloudinary.com/btdigital/image/upload/v1574379945/2-3_eq4tsg.png" />
+                <img class="clientImg" alt="Logos for Close to Home Campaign and Third Sector Communication Strategies" src="https://res.cloudinary.com/btdigital/image/upload/v1574379944/4-4_lmhap5.png" />
+                <img class="clientImg" alt="Logos for West Colfax Food Coop and Southeast Studio" src="https://res.cloudinary.com/btdigital/image/upload/v1574379945/3-3_a0eceo.png" />
             </div>
         </div>    
     </div>
